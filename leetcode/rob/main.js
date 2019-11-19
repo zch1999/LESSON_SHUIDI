f2a86dc9bd1a22fd460c947a267ff105176bfca1
zch1999 <1901394767@qq.com>
2019-11-19 22:29:33 +0800

    动态规划

--- leetcode/rob/main.js
+++ leetcode/rob/main.js
@@ -5,7 +5,8 @@ var rob = function(nums){
     for(let i=2;i<nums.length+2;i++){
         dp[i]=Math.max(dp[i-2]+nums[i-2],dp[i-1]);//i 位置上的最佳策略
     }
-    console.log(dp[nums.length+1]);
+    console.log(dp);
 }
-rob([2,7,9,3,1]);//12
+rob([1,2,1,2,1,2,1,2,6,3]);//12
 // [1,2,1,2,1,2,1,2,6,3]
+// [ 0, 0, 1, 2, 2, 4, 4, 6, 6, 8, 12, 12 ]
