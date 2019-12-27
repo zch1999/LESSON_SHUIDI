9fe9e3021352b0efad462531c7fc440583fc0606
zch1999 <1901394767@qq.com>
2019-12-27 21:40:13 +0800

    小程序状态切换

---
+++ xiaochengxu/todo/pages/index/index.js
@@ -0,0 +1,54 @@
+Page({
+  data:{
+    //todo list
+    // 全部 已完成 未完成 ...
+    lists:[], //所有的todo
+    focus:true,
+    addShow:false,
+    curLists:[], //当前状态
+    status:'1' //当前状态
+  },
+  addTodoShow(event){
+    this.setData({
+      addShow:true,
+      focus:true
+    })
+  },
+  showStatus(e){
+    var st = e.currentTarget.dataset.status;
+    if(st === this.data.status) return;
+    if(this.data.status === '1'){
+      this.setData({
+        curLists: this.data.lists,
+        status:st
+      })
+      return;
+    }
+    this.setData({
+      status:st,
+      curLists:this.data.lists.filter(item => +item.status === (st - 2))
+    })
+    this.setData({
+      status:st
+    })
+  },
+  addTodo(){
+    var temp = this.data.lists;
+    var addT = {
+      id:new Date().getTime(),
+      title:this.data.addText,
+      status:'0'
+    }
+    temp.push(addT);
+    this.setData({
+      lists:temp,
+      curLists:temp,
+      addShow:false
+    })
+  },
+  setInput(e){
+    this.setData({
+      addText:e.detail.value
+    })
+  }
+})
