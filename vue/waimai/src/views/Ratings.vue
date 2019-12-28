9fa4d3319d838077eefc95c07c939fa8e07d65cd
zch1999 <1901394767@qq.com>
2019-12-28 17:31:37 +0800

    vue

---
+++ vue/waimai/src/views/Ratings.vue
@@ -0,0 +1,19 @@
+<!--  -->
+<template>
+  <div>
+      ratings
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+
+</style>
---
+++ vue/waimai/src/views/Sellers.vue
@@ -0,0 +1,19 @@
+<!--  -->
+<template>
+  <div>
+      Sellers
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+
+</style>
