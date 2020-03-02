857394dabdc3901a98064bfbd16d312861e10d16
zch1999 <1901394767@qq.com>
2020-03-02 22:43:10 +0800

    vue三端项目

--- vue/ele_unit/ele-admin/src/api/getData.js
+++ vue/ele_unit/ele-admin/src/api/getData.js
@@ -1,14 +1,24 @@
-// api定义集合文件
-export const login = async (data)=>{
-    return new Promise((resolve,reject)=>{
-        fetch('http://127.0.0.1:3000/api/admin/login',{
-            methods:'POST',
-            body: JSON.stringify(data)
-        }).then(data =>{
-            resolve(data)
-        }).catch(err => {
-            console.log(err)
-            reject(err)
-        })
+/**
+ * API定义集合文件，
+ * @param {*} data 
+ */
+export const login = (data) => {
+    return new Promise((resolve, reject) => {
+      // http://127.0.0.1:8080/  跨项目 
+      // 跨机房 跨域了吗？
+      fetch('/api/admin/login', {
+        method: 'POST', //请求头
+        body: JSON.stringify(data) //请求体 request body
+      })
+      .then(data => data.json())
+      .then(data => {
+        console.log(data, '----------')
+        resolve(data)
+      })
+      .catch(err => {
+        console.log(err)
+        reject(err)
+      })
     })
-}
+  }
+  
--- vue/ele_unit/ele-node/routes/admin.js
+++ vue/ele_unit/ele-node/routes/admin.js
@@ -1,10 +1,12 @@
-const router = ()=>{
-    router.post('/login', (req,res)=>{
-        res.send({
-            status: 1,
-            message: '登录成功'
-        })
-    })
-}
+const express = require('express');
+const router = express.Router();
+
+router.post('/login', (req, res) => {
+  // 登录业务忽略
+  res.send({
+    status: 1,
+    message: '登录成功'
+  })
+})
 
 module.exports = router
