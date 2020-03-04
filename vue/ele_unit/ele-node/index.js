7a1eb430568c3419f5fae7ab582beaf18786d0e3
zch1999 <1901394767@qq.com>
2020-03-04 12:06:37 +0800

    项目上线服务器模拟

--- vue/ele_unit/ele-node/index.js
+++ vue/ele_unit/ele-node/index.js
@@ -1,9 +1,17 @@
-const express = require('express')
-const app = express()
-const router = require('./routes/index.js')
+const express = require('express');
+const app = express();
+const path = require('path');
+// 路由的模块化
+// api
+const router = require('./routes/index.js');
+router(app);
+// web  express.static 启用静态服务器
+app.use('/', express.static(path.join(__dirname, 'web')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/admin', express.static(path.join(__dirname, 'admin')));
 
-router(app)
 
-app.listen(3000,()=>{
-    console.log('3000')
+// app.use()
+app.listen("3000", () => {
+  console.log('api 服务器上线了');
 })
