a1f369d5d3646a9a287138fa3500b237f491d57c
zch1999 <1901394767@qq.com>
2020-03-10 20:07:13 +0800

    filter时间戳

--- vue/vue-filter-time/src/App.vue
+++ vue/vue-filter-time/src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <!-- <div id="nav"> -->
+      <!-- <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> -->
+      <x-button type="primary">button</x-button>
+    <!-- </div> -->
+    <!-- <router-view/> -->
   </div>
 </template>
 
--- vue/vue-filter-time/src/main.js
+++ vue/vue-filter-time/src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import oceanui from '@zch1999/oceanui'
+import '@zch1999/oceanui/lib/oceanui.css'
+Vue.use(oceanui)
 
 Vue.config.productionTip = false
 
