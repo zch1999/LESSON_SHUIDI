5375416211e6eba7d80d7cd5734be29999dfb094
zch1999 <1901394767@qq.com>
2020-03-26 23:42:48 +0800

    树

---
+++ vue/tree-component/src/App.vue
@@ -0,0 +1,89 @@
+<template>
+  <div id="app">
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/> -->
+    <!-- <el-tree :data="data" 
+      node-key=""
+      show-checkbox>
+
+    </el-tree> -->
+    <c-tree :data="mydata"></c-tree>
+  </div>
+</template>
+
+<script>
+export default {
+  name: App,
+  data(){
+    return {
+      data:[
+        {
+          id: 1,
+          label: '一技 1',
+          children:[{
+            id: 4,
+            label: '二级 1-1',
+            children:[{
+              id: 9,
+              label: '三级'
+            }]
+          }]
+        },
+        {
+          id: 2,
+          label: '一级 2',
+          children: [{
+            id: 5,
+            label: '二级 2-1',
+          },{
+            id: 6,
+            label: '二级 2-2',
+          }]
+        },
+        {
+          id: 3,
+          label: '一级 2',
+          children: [{
+            id: 5,
+            label: '二级 2-1',
+          },{
+            id: 6,
+            label: '二级 2-2',
+          }]
+        },
+      ],
+      mydata: [
+        {
+          
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ vue/tree-component/src/components/c-tree.vue
@@ -0,0 +1,29 @@
+<!--  -->
+<template>
+  <div class="tree-menu">
+      <ul v-for="item in data" :key="item.name">
+          <c-tree-item :data="item">
+          </c-tree-item>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'c-tree',
+  props: {
+      data: {
+          type: [Object, Array],
+          required: true
+      }
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+
+</style>
