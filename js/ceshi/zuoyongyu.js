2f673c956881ccc711e307ac67710e116e0bd9d1
zch1999 <1901394767@qq.com>
2020-04-03 22:20:52 +0800

    作用域和bind

--- js/ceshi/zuoyongyu.js
+++ js/ceshi/zuoyongyu.js
@@ -3,10 +3,14 @@ var value = 1;
 function foo() {
     console.log(value);
 }
-
+foo()
 function bar() {
-    var value = 2;
-    foo();
+    let value = 2;
+    let a = foo.bind(bar);
+    a()
 }
 
 bar();
+
+let a = new foo()
+if(a instanceof foo){console.log('true')}
