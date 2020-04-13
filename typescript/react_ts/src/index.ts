e0e24030da2284661246667b2e0c924e16413b52
zch1999 <1901394767@qq.com>
2020-04-13 17:38:54 +0800

    webpack和ts

---
+++ typescript/react_ts/src/index.ts
@@ -0,0 +1,12 @@
+// let a:number = 1;
+// console.log('a=',a)
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+
+import { HelloComponent } from './hello';
+
+
+console.log(HelloComponent)
+// ReactDom.render(
+//     <HelloComponent></HelloComponent>
+// )
