6fee898f667828bc31b1faef66306f8c58b6c122
zch1999 <1901394767@qq.com>
2020-04-22 22:15:58 +0800

    react-three

---
+++ react/react-three/src/client.js
@@ -0,0 +1,12 @@
+// console.log('hello')
+// var a = 1;
+// document.body.innerHTML = a
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+import GameContainer from './container/GameContainer'
+
+ReactDOM.render(
+  <GameContainer />,
+  document.getElementById('game')
+)
