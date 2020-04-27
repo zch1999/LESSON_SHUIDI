b260d46e46b8cd5169f5960e63c27ac6e1f1efc7
zch1999 <1901394767@qq.com>
2020-04-27 11:14:27 +0800

    逆序对,归并排序

---
+++ leetcode/m51/index.js
@@ -0,0 +1,6 @@
+//归并排序
+var reversePairs = function(nums) {
+  let end = nums.length -1
+  let start = 0
+  
+}
---
+++ leetcode/m51/index1.js
@@ -0,0 +1,12 @@
+var reversePairs = function(nums) {
+  let res = 0
+  for(let i=0;i< nums.length;i++){
+      for(let j=i+1;j<nums.length;j++){
+          if(nums[i] > nums[j]){
+              res++
+          }
+      }
+  }
+  return res
+};
+// 暴力法，leetcode超时
