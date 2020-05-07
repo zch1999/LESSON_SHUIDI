a0faf7eaf29d8bddb23e276b78110d99ff1706a9
zch1999 <1901394767@qq.com>
2020-05-07 22:28:43 +0800

    react-hook优点对比

---
+++ react/why-hook/src/App.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+function A({x, y, date}) {
+  return (
+    <div>
+      x: { x }
+      y: { y }
+      date: {date}
+    </div>
+  )
+}
+function B({x, y, date}) {
+  return (
+    <h2>
+      x: { x }
+      y: { y }
+      date: {date}
+    </h2>
+  )
+}
+function WithMosuInfo(Com) {
+  class WhithMouseComponent extends React.Component {
+    state = {
+      x: 0,
+      y: 0
+    }
+    componentDidMount() {
+      document.addEventListener('mousemove', (e) => {
+        this.setState({
+          x: e.clientX,
+          y: e.clientY
+        })
+      })
+    }
+    render() {
+      const props = this.props
+      return (<Com x={this.state.x} y={this.state.y} {...props}/>)
+    }
+  }
+  return WhithMouseComponent
+}
+// 当前坐标
+// 包装地狱
+// (<Com x={this.state.x} y={this.state.y}/>)
+// <Com date={this.state.date}/>
+// 不会自动合并
+const Amouse =  WithTime(WithMosuInfo(A));
+const Bmouse =  WithTime(WithMosuInfo(B));
+function WithTime(Com) {
+  class WithTimeCom extends React.Component {
+    state = {
+      date: new Date().toLocaleTimeString()
+    }
+    render() {
+      return (<Com date={this.state.date}/>)
+    }
+  }
+  return WithTimeCom;
+}
+function App() {
+  return (
+    <div className="App">
+     <Amouse />
+     <Bmouse />
+    </div>
+  );
+}
+
+export default App;
