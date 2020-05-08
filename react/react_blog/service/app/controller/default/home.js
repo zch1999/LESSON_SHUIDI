a3a320309d71e9559a1c5cf2b66700a15390f1a6
zch1999 <1901394767@qq.com>
2020-05-08 19:54:58 +0800

    mysql首页接口完成

--- react/react_blog/service/app/controller/default/home.js
+++ react/react_blog/service/app/controller/default/home.js
@@ -4,8 +4,24 @@ const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
   async index() {
-    const { ctx } = this;
-    ctx.body = 'api';
+    // let result = await this.app.mysql.get("blog_content",{})
+    // console.log(result)
+    // this.ctx.body = result
+    this.ctx.body = 'api hi'
+  }
+
+  async getActicleList(){
+    let sql = 'SELECT article.id as id,'+
+      'article.title as title,'+
+      'article.introduce as introduce,'+
+      'article.article_content as article_content,'+
+      "FROM_UNIXTIME(article.addTime,'%Y-%m-%d %H:%i:%s' ) as addTime ,"+
+      'article.view_count as view_count ,'+
+      'type.typeName as typeName ,'+
+      'type.id as typeId '+
+      'FROM article LEFT JOIN type ON article.type_id = type.Id '
+    const results = await this.app.mysql.query(sql)
+    this.ctx.body = {data: results}
   }
 }
 
--- react/react_blog/service/app/router/default.js
+++ react/react_blog/service/app/router/default.js
@@ -1,4 +1,6 @@
 module.exports = app => {
   const { router, controller } = app
   router.get('/default/index', controller.default.home.index)
+  router.get('/default/getActicleList', controller.default.home.getActicleList)
+
 }
