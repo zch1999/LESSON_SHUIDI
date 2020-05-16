a3d221dda672d12144ee81ac7dc8a15cf059e23e
zch1999 <1901394767@qq.com>
2020-05-16 21:29:35 +0800

    react新特性concurrent

---
+++ react/react-concurrent/src/ConcurrentDemo.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+export default class Demo extends React.Component{
+  state = {
+    length: 10000,
+    num: 1
+  }
+  updateNum = () => {
+    let num = this.state.num
+    this.setState({
+      num: num+1
+    })
+  }
+  componentDidMount(){
+    setInterval(() => {
+      this.updateNum()
+    }, 1000);
+  }
+  render() {
+    const { length,num } = this.state
+    const grids = []
+    for(let i =0; i < length; i++){
+      grids.push(
+        <div className="item" key={i}>
+          {num}
+        </div>
+      )
+    }
+    return (
+      <div className="wrapper">
+        { grids }
+      </div>
+    )
+  }
+}
