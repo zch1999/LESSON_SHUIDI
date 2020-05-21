6e919fa78754ff80c2678465128fd54929ef59b7
zch1999 <1901394767@qq.com>
2020-05-21 17:34:47 +0800

    react+electron

---
+++ electron/electron_control/app/src/main/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { ipcRenderer } from 'electron'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
